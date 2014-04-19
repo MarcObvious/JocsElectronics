01042a63daf89e8745bcad6439fc7b08d4f338e0
MarcObvious <cat166@gmail.com>
2014-04-19 19:39:40 +0200

    fitxes configuració makefile

---
+++ compilar-Linux/src/subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/camera.cpp \
+../src/controller.cpp \
+../src/entity.cpp \
+../src/entitymesh.cpp \
+../src/game.cpp \
+../src/main.cpp \
+../src/movingentity.cpp \
+../src/world.cpp 
+
+OBJS += \
+./src/camera.o \
+./src/controller.o \
+./src/entity.o \
+./src/entitymesh.o \
+./src/game.o \
+./src/main.o \
+./src/movingentity.o \
+./src/world.o 
+
+CPP_DEPS += \
+./src/camera.d \
+./src/controller.d \
+./src/entity.d \
+./src/entitymesh.d \
+./src/game.d \
+./src/main.d \
+./src/movingentity.d \
+./src/world.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
