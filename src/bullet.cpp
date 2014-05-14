03afdef413de3d7dc227f922b59b67e52e035bd0
MarcObvious <cat166@gmail.com>
2014-05-14 18:17:36 +0200

    bullet mes pesada

--- src/bullet.cpp
+++ src/bullet.cpp
@@ -2,13 +2,7 @@
 
 Bullet::Bullet(Vector3 position, Vector3 last_position, Vector3 velocity ,float ttl, float power, float author_id, std::string type){
 
-	_position = position;
-	_last_position = last_position;
-	_velocity = velocity;
-	_ttl = ttl;
-	_power = power;
-	_author_id = author_id;
-	_type = type;
+	set(position,  last_position,  velocity , ttl,  power,  author_id,  type);
 }
 
 void Bullet::set(Vector3 position, Vector3 last_position, Vector3 velocity ,float ttl, float power, float author_id, std::string type){
@@ -28,7 +22,7 @@ bool Bullet::viva() {
 
 void Bullet::update( float elapsed_time ) {
 
-	Vector3 G = Vector3(0,-50,0);
+	Vector3 G = Vector3(0,-70,0);
 	_last_position = _position;  ///Per fer colisions raig esfera (bounds)
 	_position = _position+_velocity*elapsed_time;
 	_velocity = _velocity+G*elapsed_time;
